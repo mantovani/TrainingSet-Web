48b2617f1e9c127252c9e2875c90eaab33bf2e9e
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2015-04-04 22:59:59 -0300

    removed unecessary code

--- lib/TrainingSet/Web/Schema/ResultSet/Vote.pm
+++ lib/TrainingSet/Web/Schema/ResultSet/Vote.pm
@@ -5,20 +5,4 @@ use warnings;
 
 use base 'DBIx::Class::ResultSet';
 
-sub save_votes {
-    my ( $self, $votes, $tweets, $user_id ) = @_;
-    foreach my $tweet_id ( grep { /[\w\d]{10,}/ } keys $votes ) {
-        my $insert_msg =
-          $self->resultset('Message')
-          ->find_or_create( { id => $tweet_id, text => $tweets->{$tweet_id} } );
-        $self->create(
-            {
-                user_id      => $user_id,
-                vote_type_id => $votes->{$tweet_id},
-                message_id   => $insert_msg->get_column('id'),
-            }
-        );
-    }
-}
-
 1;
